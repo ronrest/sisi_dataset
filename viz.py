b8cf5ec8a18dc2d4f3d033c4653c83b2f282d378
ronrest <ronny.coding@gmail.com>
2017-10-07 00:53:56 +1100

    DOCS: Add docstring for Add show_template_image()

--- viz.py
+++ viz.py
@@ -8,7 +8,18 @@ __license__ = "Apache License"
 __version__ = "2.0"
 
 
+# ==============================================================================
+#                                                            SHOW_TEMPLATE_IMAGE
+# ==============================================================================
 def show_template_image(x):
+    """ Given a numpy array of 0's and 1's, representing a template/mask image
+        with
+            0 = background
+            1 = region of interest
+        it returns a PIL black and white image, with:
+            white = region of interest
+            black = background
+    """
     PIL.Image.fromarray(x*255, mode="L").show()
 
 
